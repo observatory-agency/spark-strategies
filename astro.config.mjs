1de2dafd121f1d7476c7060131199de04c017e2d
Pete Lloyd <plloyd11@gmail.com>
2022-11-03 19:31:48 -0400

    image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
+
+// https://astro.build/config
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+    integrations: [
+        tailwind(),
+        svelte(),
+        image({
+            serviceEntryPoint: '@astrojs/image/sharp',
+        }),
+    ],
 });
