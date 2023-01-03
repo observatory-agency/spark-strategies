b52dc56189ed7f1e74cc6c0ec7f22bdf5d7d9c5d
Pete Lloyd <plloyd11@gmail.com>
2023-01-03 18:05:37 -0500

    new nav items

--- src/components/ui/Header.svelte
+++ src/components/ui/Header.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { slide } from 'svelte/transition';
+    import { slide, fade } from 'svelte/transition';
     import { cubicOut } from 'svelte/easing';
 
     let navItems = [
@@ -10,11 +10,15 @@
         },
         { name: 'About Us', anchor: '/#about' },
         { name: 'Free Mini Trainings', anchor: '/#trainings' },
-        { name: 'Media', anchor: '/media-library' },
+    ];
+    let moreItems = [
+        { name: 'Resources', anchor: 'https://resources.sparkstrategies.co/' },
+        { name: 'Media Library', anchor: '/media-library' },
         { name: 'Blog', anchor: '/blog' },
     ];
 
     let navOpen = false;
+    let moreOpen = false;
 
     let offset = 0;
     let tolerance = 0;
@@ -64,7 +68,7 @@
 <header class="fixed top-0 z-[100] w-full {headerClass}">
     <nav class="relative mx-auto max-w-7xl max-xl:px-4" aria-label="Top">
         <div class="flex items-center justify-between w-full py-4">
-            <div class="flex items-center space-x-8">
+            <div class="flex items-center space-x-6">
                 <a href="/">
                     <span class="sr-only">Spark Strategies</span>
                     <svg
@@ -95,11 +99,132 @@
                         />
                     </svg>
                 </a>
-                <div class="hidden gap-4 text-lg lg:flex">
+                <div class="hidden gap-6 text-lg lg:flex">
                     {#each navItems as navItem}
                         <a href={navItem.anchor}>{navItem.name}</a>
                     {/each}
                 </div>
+                <button
+                    type="button"
+                    class="items-center hidden rounded-md lg:inline-flex group hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-sparkBlue focus:ring-offset-2"
+                    aria-expanded="false"
+                    on:click={() => (moreOpen = !moreOpen)}
+                >
+                    <span class="text-lg text-sparkBody">More</span>
+                    <svg
+                        class="w-5 h-5 ml-2 text-gray-600 group-hover:text-gray-500"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                            clip-rule="evenodd"
+                        />
+                    </svg>
+                </button>
+                {#if moreOpen}
+                    <div class="relative" transition:fade={{ duration: 250 }}>
+                        <div
+                            class="absolute z-10 w-screen max-w-md px-2 mt-8 transform -translate-x-2/3 left-1/2 sm:px-0"
+                            transition:slide={{ y: 150, duration: 300, easing: cubicOut }}
+                        >
+                            <div
+                                class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5"
+                            >
+                                <div class="relative grid gap-6 px-5 py-6 bg-white sm:gap-8 sm:p-8">
+                                    <a
+                                        href="https://resources.sparkstrategies.co"
+                                        class="flex items-start p-3 -m-3 rounded-lg hover:bg-gray-50"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke-width="1.5"
+                                            stroke="currentColor"
+                                            class="flex-shrink-0 w-6 h-6 text-sparkBlue"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+                                            />
+                                        </svg>
+
+                                        <div class="ml-4">
+                                            <p class="text-base font-medium text-gray-900">
+                                                Resources
+                                            </p>
+                                            <p class="mt-1 text-sm text-gray-500">
+                                                Sign up for our members only resource center
+                                            </p>
+                                        </div>
+                                    </a>
+
+                                    <a
+                                        href="/media-library"
+                                        class="flex items-start p-3 -m-3 rounded-lg hover:bg-gray-50"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke-width="1.5"
+                                            stroke="currentColor"
+                                            class="flex-shrink-0 w-6 h-6 text-sparkBlue"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0118 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0118 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 016 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5"
+                                            />
+                                        </svg>
+
+                                        <div class="ml-4">
+                                            <p class="text-base font-medium text-gray-900">
+                                                Media Library
+                                            </p>
+                                            <p class="mt-1 text-sm text-gray-500">
+                                                Check out our latest videos and podcasts
+                                            </p>
+                                        </div>
+                                    </a>
+
+                                    <a
+                                        href="/blog"
+                                        class="flex items-start p-3 -m-3 rounded-lg hover:bg-gray-50"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke-width="1.5"
+                                            stroke="currentColor"
+                                            class="flex-shrink-0 w-6 h-6 text-sparkBlue"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"
+                                            />
+                                        </svg>
+
+                                        <div class="ml-4">
+                                            <p class="text-base font-medium text-gray-900">Blog</p>
+                                            <p class="mt-1 text-sm text-gray-500">
+                                                Check out our Profitable Studio Blog for the latest
+                                                in the industry.
+                                            </p>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                {/if}
             </div>
             <div class="flex items-center gap-4 sm:gap-8">
                 <a
@@ -130,6 +255,15 @@
                 >
                     <div class="flex flex-col items-start space-y-4">
                         {#each navItems as navItem}
+                            <a
+                                on:click|once={toggleNav}
+                                href={navItem.anchor}
+                                class="text-lg font-medium text-primaryColor font-body"
+                            >
+                                {navItem.name}
+                            </a>
+                        {/each}
+                        {#each moreItems as navItem}
                             <a
                                 on:click|once={toggleNav}
                                 href={navItem.anchor}
